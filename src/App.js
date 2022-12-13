f36d3662792753bd5e8566d7c769038b709b7da4
Kristof Kruller <kristof.kruller@gmail.com>
2022-12-13 07:01:51 +0100

    logo, routing architecture basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Categories from "./components/Categories";
+import Navigation from "./components/Navigation";
+import Shop from "./components/Shop";
+
 
 const App = () => {
 
   return (
-    <Categories />
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Categories />}/>
+        <Route path="shop" element={<Shop />}/>
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import styled from 'styled-components';
+import {ReactComponent as Logo} from "../assets/crown.svg"
+
+const Nav = styled.section`
+    height: 70px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 25px;
+  
+    .logo-container {
+      height: 100%;
+      width: 70px;
+      padding: 25px;
+    }
+  
+    .nav-links-container {
+      width: 50%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+  
+      .nav-link {
+        padding: 10px 15px;
+        cursor: pointer;
+      }
+    }
+`
+
+const Navigation = () => {
+  return (
+    <>
+        <Nav className='navigation'>
+            <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+                <Link className='nav-link' to='/sign-in'>
+                    SIGN IN
+                </Link>
+            </div>
+        </Nav>
+        <Outlet />
+    </>
+  )
+}
+
+export default Navigation
