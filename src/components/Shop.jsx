c9853b35cc1dcbafb93bcdce4a88f52cbc933a1c
Kristof Kruller <kristof.kruller@gmail.com>
2022-12-21 07:13:47 +0100

    shop json, component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import ShopData from "../assets/shop-data.json"
 
 const ShopWrap = styled.section`
 
@@ -7,10 +8,18 @@ const ShopWrap = styled.section`
 
 const Shop = () => {
 
-  
+  console.log(ShopData[0].name)
   return (
-    <ShopWrap>
-    </ShopWrap>
+    <>
+      {ShopData.map(({  id, name  }) => (
+
+        <div key={id}>
+          <h1>{name}</h1>
+        </div>
+      
+      
+      ))}
+    </>
   )
 }
 
